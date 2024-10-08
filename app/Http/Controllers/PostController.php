0cb12cf06de332f44b5b524d750360cc7d2cb7c9
yutak215 <yuta.k.215@gmail.com>
2024-10-09 08:16:46 +0900

    データベース連携

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // postsテーブルからすべてのデータを取得し、変数$postsに代入する
+        $posts = DB::table('posts')->get();
+
+        // 変数$productsをposts/index.blade.phpファイルに渡す
+        return view('posts.index', compact('posts'));
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,6 +9,18 @@
  
  <body>
     <h1>投稿一覧</h1>
+    <table>
+        <tr>
+            <th>タイトル</th>
+            <th>本文</th>
+        </tr>
+        @foreach($posts as $post)
+        <tr>
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->content }}</td>
+        </tr>
+        @endforeach
+    </table>
  </body>
  
  </html>
