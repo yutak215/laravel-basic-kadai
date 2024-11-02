fbf07b17453fdd37df886a29484b65a05bc8644f
yutak215 <yuta.k.215@gmail.com>
2024-11-02 09:46:59 +0900

    a

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\SignInController;
+use App\Http\Controllers\CookieController;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +26,56 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/posts/create', [PostController::class, 'create']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/posts/{id}', [PostController::class, 'show']);
+require __DIR__.'/auth.php';
 
-
-
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::get('/hello', [HelloController::class, 'index']);
+ 
+ Route::get('/products', [ProductController::class, 'index']);
+ 
+ Route::get('/products/create', [ProductController::class, 'create']);
+ 
+ Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+ 
+ Route::get('/products/{id}', [ProductController::class, 'show']);
+ 
+ Route::get('/vendors/create', [VendorController::class, 'create']);
+ 
+ Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
+ 
+ Route::get('/vendors/{id}', [VendorController::class, 'show']);
+ 
+ Route::get('/requests/create', [RequestController::class, 'create']);
+ 
+ Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
+ 
+ Route::get('/responses', [ResponseController::class, 'index']);
+ 
+ Route::get('/sign-in', [SignInController::class, 'create']);
+ 
+ Route::post('/sign-in', [SignInController::class, 'store'])->name('sign-in.store');
+ 
+ Route::get('/cookies', [CookieController::class, 'index']);
+ 
+ Route::get('/cookies/create', [CookieController::class, 'create'])->name('cookies.create');
+ 
+ Route::post('/cookies/store', [CookieController::class, 'store'])->name('cookies.store');
+ 
+ Route::delete('/cookies/destroy', [CookieController::class, 'destroy'])->name('cookies.destroy');
+ 
+ Route::get('/sessions', [SessionController::class, 'index']);
+ 
+ Route::get('/sessions/create', [SessionController::class, 'create'])->name('sessions.create');
+ 
+ Route::post('/sessions/store', [SessionController::class, 'store'])->name('sessions.store');
+ 
+ Route::delete('/sessions/destroy', [SessionController::class, 'destroy'])->name('sessions.destroy');
